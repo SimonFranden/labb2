67262646ac08c660d30a0f726d156acc9de8b0dd
Simon Frändén <simon.franden@hotmail.com>
2023-03-27 00:09:27 +0200

    finished labb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import NavBar from './NavBar';
+import SideNav from './SideNav'
+import Home from './Home'
+import About from "./About";
+import Cv from "./Cv";
+import './style.css'
+import Portfolio from "./Portfolio";
+import GithubFeed from "./GithubFeed";
+import PopupEasteregg from "./PopupEasteregg";
+
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+    <div className='App'>
+           
+      <Router>
+        <header>
+          <p>SimonFrändén</p>
+          <a href="#navbar" className="burger fa-solid fa-bars"></a>
+          <NavBar/>  
+        </header>
+        <SideNav/>
+        
+        <div class="content-ctn" >
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/About" element={<About/>} />
+            <Route path="/Cv" element={<Cv/>} />
+            <Route path="Portfolio" element={<GithubFeed/>} />
+          </Routes>
+        </div>
+        </Router>      
+        <PopupEasteregg></PopupEasteregg>
     </div>
+    
   );
 }
 
 export default App;
+
